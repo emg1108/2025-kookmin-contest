4614c50d79d46eb8304960da8cb42960bb51b15c
sig <emg1108@naver.com>
2025-07-24 12:37:41 +0900

    Add files via upload

---
+++ install/motor_node_cpp/share/motor_node_cpp/launch/motor_node.launch.py
@@ -0,0 +1,38 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    motor_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('xycar_motor'),
+                'launch/xycar_motor.launch.py'))
+    )
+
+    motor_publisher_node = Node(
+        package='motor_node_cpp',
+        executable='motor_node',
+        name='motor_node',
+        output='screen'
+    )
+
+    # auto_drive 실행파일 절대 경로 계산
+    auto_drive_path = "/home/xytron/osy_250720/2025-SEAME/auto_drive"
+    auto_drive_process = ExecuteProcess(
+        cmd=[auto_drive_path, "d"],
+        output='screen',
+        additional_env={
+            'LD_LIBRARY_PATH': '/home/xytron/osy_250720/2025-SEAME:' + os.environ.get('LD_LIBRARY_PATH', '')
+        }
+    )
+
+
+    return LaunchDescription([
+        motor_include,
+        motor_publisher_node,
+        auto_drive_process
+    ])
